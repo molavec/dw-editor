4f64a077da31d85d6ac1a25ac79bd9f511e094a2
Miguel Olave <molavec@gmail.com>
2023-05-01 15:23:33 -0400

    tailwind added to storybook

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -2,11 +2,13 @@ import type { Meta, StoryObj } from '@storybook/vue3';
 
 import Button from './Button.vue';
 
-// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+// More on how to set up stories at:
+// https://storybook.js.org/docs/vue/writing-stories/introduction
 const meta = {
   title: 'Example/Button',
   component: Button,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+  // This component will have an automatically generated docsPage entry:
+  // https://storybook.js.org/docs/vue/writing-docs/autodocs
   tags: ['autodocs'],
   argTypes: {
     size: { control: 'select', options: ['small', 'medium', 'large'] },
@@ -19,7 +21,8 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 /*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ *👇 Render functions are a framework specific feature
+ * to allow you control on how the component renders.
  * See https://storybook.js.org/docs/vue/api/csf
  * to learn how to use render functions.
  */
